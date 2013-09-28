385366f9acfcc2d179673c03999987da49b536fa
Zach Nichols <Zach@Zachs-Mac-mini.local>
2013-09-28 16:35:58 -0400

    Initial Commit

---
+++ Flora Dummy/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Flora Dummy
+//
+//  Created by Zach Nichols on 9/28/13.
+//  Copyright (c) 2013 SGSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
